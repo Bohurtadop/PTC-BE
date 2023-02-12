6bac2d98b1d31a855d79142e313f1097181bf3b1
bohurtadop <bohurtadop@unal.edu.co>
2023-02-11 22:54:00 -0500

    Update index

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 import express from 'express';
+import client from './controllers/dabatase.js';
+import questionsRouter from './routes/questions.js';
+import companiesRouter from './routes/companies.js';
 
 const server = express();
 
-server.listen(3000, () => {
-    console.log('server listen on port 3000');
+await client.connect();
+console.log('database connected');
+
+server.listen(3001, () => {
+    console.log('server listen on port 3001');
 });
 
 server.get('/', (req, res) => {
     res.send('Hello world');
 })
+
+server.use('/questions', questionsRouter);
+server.use('/companies', companiesRouter);
